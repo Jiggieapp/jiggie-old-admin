3b3a5bb53c2d48473e75820206fae33f34fe5b84
Sunny Clark <sunny@sunnyclark.com>
2015-11-19 17:48:51 +0700

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -2358,7 +2358,7 @@ function getEventDetailPage(url,type){
                 console.log("resp.start_datetime " + resp.start_datetime)
                 $('#edit_startdatetime').editable({  
                     type:'combodate',
-                    value: new Date(resp.start_datetime),
+                    value: resp.start_datetime,
                     minuteStep:10,
                     url: base_url+'admin/events/'+updatetype+'/'+$("#selected_event").attr("_id"),
                     pk: 1
